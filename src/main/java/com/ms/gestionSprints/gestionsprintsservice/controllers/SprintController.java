6b08fbd950849d78530f34fa478a3800b2dd7023
Sinda <sindahaboubi104@gmail.com>
2023-04-14 21:21:50 +0100

    gestionSprintsS

--- src/main/java/com/ms/gestionSprints/gestionsprintsservice/controllers/SprintController.java
+++ src/main/java/com/ms/gestionSprints/gestionsprintsservice/controllers/SprintController.java
@@ -21,32 +21,21 @@ public class SprintController {
     private SprintService sprintService;
     @Autowired
     private ProductBacklogService productBacklogService;
-    @Autowired
-    private SprintRepository sprintRepository;
-    @GetMapping
-    public List<Sprint> getAllSprints() {
-        return sprintService.findAllSprints();
-    }
-
     @GetMapping("/productBacklog/{id}")
     public List<Sprint> getSprintsByProductBacklog(@PathVariable(name="id") Long id) throws SQLException {
         ProductBacklog productBacklog  = this.productBacklogService.findProductBacklogById(id);
         List<Sprint> sprints = this.sprintService.findAllSprintsByProductBacklog(id);
-
         Collections.sort(sprints, new Comparator<Sprint>() {
             @Override
             public int compare(Sprint s1, Sprint s2) {
                 return s1.getDateFin().compareTo(s2.getDateFin());
             }
         });
-
         for(Sprint sprint:sprints){
             sprint.setProductBacklog(productBacklog);
         }
         return sprints;
     }
-
-
     @PostMapping
     public ResponseEntity<Sprint> createSprint(@RequestBody Sprint sprint, @RequestParam Long productBacklogId) {
         sprint.setProductBacklogId(productBacklogId);
@@ -55,7 +44,6 @@ public class SprintController {
         Sprint createdSprint = sprintService.createSprint(sprint);
         return new ResponseEntity<>(createdSprint, HttpStatus.CREATED);
     }
-
     @PutMapping
     public Sprint modifierSprintDate(@RequestBody Sprint sprint){
         System.out.println(sprint);
@@ -63,12 +51,10 @@ public class SprintController {
         sprint.setProductBacklog(productBacklog);
         return this.sprintService.modifierSprint(sprint);
     }
-
     @DeleteMapping("/{id}")
     public void supprimerSprint(@PathVariable("id") Long id){
         this.sprintService.supprimerSprint(id);
     }
-
     @GetMapping("/{id}")
     public Sprint getSprint(@PathVariable("id") Long id){
         return this.sprintService.getSprint(id);
